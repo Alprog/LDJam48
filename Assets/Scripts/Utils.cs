ee63a829dab62faa7c30f5687d22b6d199fed702
Alexander Tuzhik <alprog@hotmail.com>
2021-04-24 23:20:02 +0200

    wip

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -3,18 +3,5 @@ using UnityEngine;
 
 class Utils
 {
-    public static void RefreshEditors()
-    {
-        var editors = Resources.FindObjectsOfTypeAll<UnityEditor.Editor>();
-        foreach (var editor in editors)
-        {
-            editor.Repaint();
-        }
 
-        var windows = Resources.FindObjectsOfTypeAll<UnityEditor.EditorWindow>();
-        foreach (var window in windows)
-        {
-            window.Repaint();
-        }
-    }
 }
