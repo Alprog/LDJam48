832b43a5f81d0041b35bf3f2b837db1b7dca1672
Alexander Tuzhik <alprog@hotmail.com>
2021-04-27 02:37:39 +0200

    wip

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -6,7 +6,7 @@ public static class Data
     public static float Gold = 0;
     public static float Coal = 0;
     public static float Health = 100;
-    public static int RMBShots = 5;
+    public static int RMBShots = 20;
     public static List<GnomeData> GnomeDatas = CreateGnomeDatas(3);
 
     private static List<GnomeData> CreateGnomeDatas(int count)
